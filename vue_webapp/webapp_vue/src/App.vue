cf9a1f8d9c26b3d8ae5d1b15f385b6218d8a8e3c
Javier Marín Guimerà <marinj460@gmail.com>
2023-12-09 19:32:04 +0100

    Tailwind added + router updated + nav created

--- vue_webapp/webapp_vue/src/App.vue
+++ vue_webapp/webapp_vue/src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/helloworld.vue'
+import { RouterView } from 'vue-router'
 import MyHeader from './components/MyHeader/MyHeader.vue'
 </script>
 
+<style src="./assets/main.css"></style>
+
 <template>
   <MyHeader/>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
--- vue_webapp/webapp_vue/src/router/router.js
+++ vue_webapp/webapp_vue/src/router/router.js
@@ -4,22 +4,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      // Home view
       path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
+      name: 'Redirect to home',
+      redirect: "/home",
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }, 
+      path: '/home',
+      name: 'Home',
+      
+      component: () => import('../views/home.vue')
+    },
     {
       path: '/test',
-      name: 'hola buenos días',
+      name: 'Test',
 
       component: () => import('../views/test.vue')
     }
