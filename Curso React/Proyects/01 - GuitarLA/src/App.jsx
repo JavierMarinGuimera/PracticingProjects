8c13a50473b628ea5eb218b3353bffdc85051d1b
Javier Marín Guimerà <marinj460@gmail.com>
2026-01-30 15:34:56 +0100

    Video 61 done

--- Curso React/Proyects/01 - GuitarLA/src/App.jsx
+++ Curso React/Proyects/01 - GuitarLA/src/App.jsx
@@ -1,9 +1,33 @@
-function App() {
+import React from "react";
+import Header from "./Components/Header.jsx";
+import Guitar from "./Components/Guitar.jsx";
+
+export default function App() {
+
+  // States
+  const [auth, setAuth] = React.useState(false);
+  const [total, setTotal] = React.useState(0);
+  const [cart, setCart] = React.useState([]);
+
   return (
     <>
-      <h1>GuitarLA</h1>
-    </>
-  )
-}
+      <Header />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
 
-export default App
+        <div className="row mt-5">
+          {/* Aquí irán las guitarras */}
+          <Guitar />
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
