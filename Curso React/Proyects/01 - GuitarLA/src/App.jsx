347d2025162a49c812508de97ec7cfd74cde7c4c
Javier Marín <marinj460@gmail.com>
2026-02-02 22:02:25 +0100

    Episode 81 done

--- Curso React/Proyects/01 - GuitarLA/src/App.jsx
+++ Curso React/Proyects/01 - GuitarLA/src/App.jsx
@@ -1,23 +1,66 @@
 import React from "react";
 import Header from "./Components/Header.jsx";
 import Guitar from "./Components/Guitar.jsx";
+import { db } from "./data/db.js";
 
 export default function App() {
-
-  // States
-  const [auth, setAuth] = React.useState(false);
-  const [total, setTotal] = React.useState(0);
+  const [data, setData] = React.useState(db);
   const [cart, setCart] = React.useState([]);
+
+  function addToCart(item) {
+    const alreadyInCart = cart.find((product) => product.id === item.id);
+
+    if (alreadyInCart) {
+      const updatedCart = cart.map((product) => {
+        if (product.id === item.id) {
+          return { ...product, quantity: product.quantity + 1 };
+        }
+        return product;
+      });
+
+      setCart(updatedCart);
+    } else {
+      setCart([...cart, { ...item, quantity: 1 }]);
+    }
+  }
+
+  function updateQuantity(id, newQuantity) {
+    setCart((prevCart) => {
+      if (newQuantity < 1) {
+        return prevCart.filter((item) => item.id !== id);
+      }
+      return prevCart.map((item) =>
+        item.id === id ? { ...item, quantity: newQuantity } : item,
+      );
+    });
+  }
+
+  function saveToLocalStorage() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  React.useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    saveToLocalStorage();
+  }, [cart]);
 
   return (
     <>
-      <Header />
+      <Header cart={cart} setCart={setCart} updateQuantity={updateQuantity} />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
           {/* Aquí irán las guitarras */}
-          <Guitar />
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
         </div>
       </main>
 
--- Curso React/Proyects/01 - GuitarLA/src/Components/Header.jsx
+++ Curso React/Proyects/01 - GuitarLA/src/Components/Header.jsx
@@ -1,85 +1,127 @@
-import React from 'react';
+import React from "react";
 
-export default function Header() {
-    return (
-        <header className="py-5 header">
-            <div className="container-xl">
-                <div className="row justify-content-center justify-content-md-between">
-                    <div className="col-8 col-md-3">
-                        <a href="index.html">
-                            <img
+export default function Header({ cart, setCart, updateQuantity }) {
+  const isEmpty = React.useMemo(() => cart.length === 0, [cart]);
+
+  const totalItems = React.useMemo(
+    () => cart.reduce((total, item) => total + item.quantity, 0),
+    [cart],
+  );
+
+  const totalPrice = React.useMemo(
+    () => cart.reduce((total, item) => total + item.price * item.quantity, 0),
+    [cart],
+  );
+
+  return (
+    <header className="py-5 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="index.html">
+              <img
+                className="img-fluid"
+                src="./img/logo.svg"
+                alt="imagen logo"
+              />
+            </a>
+          </div>
+          <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+            <div className="carrito">
+              <img
+                className="img-fluid"
+                src="./img/carrito.png"
+                alt="imagen carrito"
+              />
+
+              <div id="carrito" className="bg-white p-3">
+                {isEmpty ? (
+                  <p className="text-center">El carrito esta vacio</p>
+                ) : (
+                  <div>
+                    <p className="text-center">
+                      Carrito con {totalItems} productos
+                    </p>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {cart.map((item) => (
+                          <tr key={item.id}>
+                            <td>
+                              <img
                                 className="img-fluid"
-                                src="./img/logo.svg"
-                                alt="imagen logo"
-                            />
-                        </a>
+                                src={`/img/${item.image}.jpg`}
+                                alt={`imagen ${item.name}`}
+                              />
+                            </td>
+                            <td>{item.name}</td>
+                            <td className="fw-bold">${item.price}</td>
+                            <td className="flex align-items-center gap-4">
+                              <button
+                                type="button"
+                                className="btn btn-dark px-4"
+                                disabled={item.quantity <= 1}
+                                onClick={() =>
+                                  updateQuantity(item.id, item.quantity - 1)
+                                }
+                              >
+                                -
+                              </button>
+                              {item.quantity}
+                              <button
+                                type="button"
+                                className="btn btn-dark px-3"
+                                onClick={() =>
+                                  updateQuantity(item.id, item.quantity + 1)
+                                }
+                              >
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button
+                                className="btn btn-danger"
+                                type="button"
+                                onClick={() =>  updateQuantity(item.id, 0)}
+                              >
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+
+                    <p className="text-end">
+                      Total pagar:
+                      {<span className="fw-bold">${totalPrice}</span>}
+                    </p>
+
+                    {/* Botón para vaciar el carrito */}
+                    <div className="text-center mt-3">
+                      <button
+                        type="button"
+                        className="btn btn-dark bg-danger p-2"
+                        onClick={() => setCart([])}
+                      >
+                        Vaciar Carrito
+                      </button>
                     </div>
-                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                        <div className="carrito">
-                            <img
-                                className="img-fluid"
-                                src="./img/carrito.png"
-                                alt="imagen carrito"
-                            />
-
-                            <div id="carrito" className="bg-white p-3">
-                                <p className="text-center">El carrito esta vacio</p>
-                                <table className="w-100 table">
-                                    <thead>
-                                        <tr>
-                                            <th>Imagen</th>
-                                            <th>Nombre</th>
-                                            <th>Precio</th>
-                                            <th>Cantidad</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <img
-                                                    className="img-fluid"
-                                                    src="./img/guitarra_02.jpg"
-                                                    alt="imagen guitarra"
-                                                />
-                                            </td>
-                                            <td>SRV</td>
-                                            <td className="fw-bold">$299</td>
-                                            <td className="flex align-items-start gap-4">
-                                                <button type="button" className="btn btn-dark">
-                                                    -
-                                                </button>
-                                                1
-                                                <button type="button" className="btn btn-dark">
-                                                    +
-                                                </button>
-                                            </td>
-                                            <td>
-                                                <button className="btn btn-danger" type="button">
-                                                    X
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                                <p className="text-end">
-                                    Total pagar: 
-                                    {<span className='fw-bold'>$899</span>}
-                                </p>
-
-                                {/* Botón para vaciar el carrito */}
-                                {/* Aquí iría el botón para vaciar el carrito */}
-                            </div>
-
-                        </div>
-
-                    </nav>
-
-                </div>
-
+                  </div>
+                )}
+              </div>
             </div>
-
-        </header >
-    );
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
