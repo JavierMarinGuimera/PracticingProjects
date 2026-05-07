9f78fc098e6806bd0068c8325661af0347951616
Javier Marín <marinj460@gmail.com>
2026-05-07 17:05:58 +0200

    Some files that had to be uploaded

---
+++ Projects/economics_app/economics_website/.next/static/chunks/app/history/page-910cc501e061ab7a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[811,702,202],{7886:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7886)}),_N_E=n.O()}]);
