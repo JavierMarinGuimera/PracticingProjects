d9cc8a335613b6bdc4c06c52f59b2561dd2d1000
Javier Marín <marinj460@gmail.com>
2026-06-28 13:17:57 +0200

    Proyects to projects folder name 2

--- Projects/economics_app/economics_website/.next/server/webpack-runtime.js
+++ Projects/economics_app/economics_website/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3df1cd34cf369072")
+/******/ 		__webpack_require__.h = () => ("363eb54bed531c1e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
